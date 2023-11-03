41b0d7d886d704c957cfe71bca2613813f4ae350
fabiogabrielli <fabiomgabrielli@gmail.com>
2023-11-03 17:18:05 -0300

    links MENU

--- menuGeral.php
+++ menuGeral.php
@@ -4,10 +4,10 @@
     <div class="logo"><img class="imgLogo" src="imgs/ctres-logotipo-misto.svg" alt="logotipo-ctres"></div>
     <div class="nave">
       <a href="index.php">INÍCIO</a>
-      <a href="/ctres/index.php#somos">QUEM SOMOS</a>
+      <a href="/index.php#somos">QUEM SOMOS</a>
       <a href="somos.php">30 ANOS DE HISTÓRIA</a>
       <a href="#">PRODUTOS E SERVIÇOS</a>
-      <a href="/ctres/index.php#secArtigos">CONTEÚDO (BLOG)</a>
+      <a href="/index.php#secArtigos">CONTEÚDO (BLOG)</a>
     </div>
     <div class="redessociais">
       <a href="https://www.facebook.com/grupoctresoficial/"><i class="fa-brands fa-square-facebook"></i></a>

03c44fb8c521b59946198f348bdc1fdbc981e2c4
fabiogabrielli <fabiomgabrielli@gmail.com>
2023-12-12 12:06:12 -0300

    teste

--- js.js
+++ js.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 2
+            slidesToShow: 1
           }
         },
         {
